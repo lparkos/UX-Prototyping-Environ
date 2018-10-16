3bf19835f8a67c5c6571fdedcc00ed6edb7c0165
Lauren <lauren.parkos@gmail.com>
2018-10-15 21:38:45 -0500

    Initializing

---
+++ README.md
@@ -0,0 +1,22 @@
+# Gulpazan
+Gulpazan is a Gulp starter template to build a fast and easy workflows. It comes with Gulp, Babel, Nunjucks, Bootstrap, jQuery, and Sass to make it super easy for you to create a better workflow.
+
+## What's included?
+What prominent npm packages Gulpazan comes with when you install it...
+
+- Gulp: The toolkit for automating painful or time-consuming tasks in your development workflow, so you can stop messing around and build something.
+- Babel: Babel is a very lovely tool to transpile your ES6 code into ES5. So, Use next generation JavaScript, today!
+- jQuery: It is a fast, small, and feature-rich JavaScript library.
+- Bootstrap: The most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.
+- Nunjucks: One of the most powerful templating languages for JavaScript.
+- Ionicons: The premium icon font for Ionic Framework.
+
+## How to install Gulpazan?
+
+Clone the repository by typing `git clone https://github.com/babilkuyusu/Gulpazan.git` command in your terminal window or [click here](https://github.com/babilkuyusu/Gulpazan/archive/master.zip) to download Gulpazan in to your local computer.
+
+Open your terminal window and type `npm install` command to install all the dependencies that Gulpazan needs.
+
+After the installation is finished type `gulp start` command and hit the enter to power up the project.
+
+Easy, isn't it?
---
+++ gulpfile.js
@@ -0,0 +1,129 @@
+var gulp = require('gulp'); // Gulp çağırılıyor.
+var sass = require('gulp-sass'); // SASS compiler
+var browserSync = require('browser-sync').create(); // Browser synchronization for watch...
+var cache = require('gulp-cache'); //C ache'i temizler.
+var del = require('del'); // Dist klasörünü temizlemek ve tamamen kaldırmak için kullanılır.
+var runSequence = require('run-sequence'); // Taskların çalışma önceliğini belirler...
+var autoprefixer = require('gulp-autoprefixer'); // Adding vendor prefixes to the CSS files
+var sourcemaps = require('gulp-sourcemaps'); // dist klasöründeki CSS çıktısına sourcemap ekliyor!
+var nunjucksRender = require('gulp-nunjucks-render'); // HTML templating için kullanılıyor.
+var handlebars = require('gulp-handlebars');
+var defineModule = require('gulp-define-module');
+var babel = require('gulp-babel'); // ES6 code in, ES5 code out!
+
+// Babel, scripts.js dosyasındaki ES6 kodları ES5'e transpile ediyor ve babelified klasörüne atıyor!
+gulp.task('babelify', () =>
+  gulp.src('src/assets/js/scripts.js')
+  .pipe(babel({
+    presets: ['@babel/env']
+  }))
+  .pipe(gulp.dest('src/assets/js/babelified'))
+);
+
+// Nunjucks HTML Templating
+gulp.task('nunjucks', function() {
+  // Gets .html and .njk files in pages folder...
+  return gulp.src('src/pages/**/*.+(html|njk)')
+  // Renders template folder with nunjucks...
+  .pipe(nunjucksRender({
+      path: ['src/templates']
+    }))
+  // output files in src folder
+  .pipe(gulp.dest('src'))
+});
+
+gulp.task('handlebars', function(){
+  gulp.src(['src/pages/**/*.hbs'])
+    .pipe(handlebars())
+    .pipe(defineModule('node'))
+    .pipe(gulp.dest('src'));
+});
+
+// Sass Compile
+gulp.task('sass', function() {
+  return gulp.src('src/assets/sass/**/*.scss')
+  // gulp-sass kullanarak Sass dosyasını CSS'e çeviriyor. (nested, compact, expanded, compressed)
+  .pipe(sourcemaps.init())
+  .pipe(sass({outputStyle: 'expanded'})).on("error", function swallowError (error) {
+    console.log(error.toString())
+    this.emit('end')
+  })
+  .pipe(sourcemaps.write())
+  .pipe(autoprefixer({browsers: ['last 1 version', 'iOS 6'], cascade: false})) // CSS dosyasına prefixler ekleniyor...
+  .pipe(gulp.dest('src/assets/css'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+});
+
+// BrowserSync'i çalıştırıyor.
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'src'
+    },
+  })
+});
+
+// Imajları dist klasörüne taşır.
+gulp.task('images', function(){
+  return gulp.src('src/assets/images/**/*.+(png|jpg|gif|svg)')
+  .pipe(gulp.dest('dist/assets/images'))
+});
+
+// Fontları dist klasörüne taşır.
+gulp.task('fonts', function(){
+  return gulp.src('src/assets/fonts/**/*')
+  .pipe(gulp.dest('dist/assets/fonts'))
+});
+
+// CSS dosyalarını dist klasörüne taşır.
+gulp.task('css', function(){
+  return gulp.src('src/assets/css/**/*.css')
+    .pipe(gulp.dest('dist/assets/css/'));
+});
+
+// Transpile edilmiş scripts.js dosyasını dist klasörüne taşır.
+gulp.task('js', function(){
+  return gulp.src([
+    // Burada sıralama çok önemlidir.
+    'src/assets/js/*.js', // İlk olarak tüm js dosyalarını taşır.
+    'src/assets/js/babelified/scripts.js' // İkinci olarak da transpile edilmiş scripts.js dosyasını orijinalin üzerine yazar!
+  ])
+    .pipe(gulp.dest('dist/assets/js/'));
+});
+
+// HTML dosyalarını dist klasörüne taşır.
+gulp.task('html', function(){
+  return gulp.src('src/*.html')
+  .pipe(gulp.dest('dist'))
+});
+
+// Dist klasörünü temizlemek, tamamen kaldırmak için...
+gulp.task('clean:dist', function(){
+  return del.sync('dist');
+});
+
+// Cache'i temizler...
+gulp.task('cache:clear', function (callback) {
+  return cache.clearAll(callback)
+})
+
+// Watching
+//gulp.watch('files-to-watch', ['task-to-run']);
+gulp.task('start', ['browserSync', 'babelify', 'sass', 'handlebars'], function(){
+  gulp.watch('src/**/*.hbs)', ['handlebars']);
+  gulp.watch('src/assets/sass/**/*.scss', ['sass']);
+  gulp.watch('src/assets/js/**/*.js', ['babelify']);
+  gulp.watch('src/assets/js/**/*.js', browserSync.reload);
+  gulp.watch('src/*.html', browserSync.reload);
+  // İzlemek istediğiniz diğer uglamalar.
+});
+
+// Tasklar, çalışma önceliğine göre sıraya konuluyor...
+gulp.task('build', function(callback) {
+  runSequence('clean:dist',
+    ['js', 'sass', 'css', 'images', 'fonts', 'html'],
+    callback
+  )
+});
